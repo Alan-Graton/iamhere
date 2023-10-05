9eee9fe52d9b520d24b49278e220417521e1c57a
Alan-Graton <graton.alan@gmail.com>
2023-10-05 09:17:30 -0300

    wip: add new asset and delete action is working

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 // Widgets
 import {
+  Image,
   ScrollView,
   Text,
   TextInput,
@@ -11,16 +12,32 @@ import { Checkbox, IconButton } from "react-native-paper";
 // Styles
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from "./styles";
+import Toast from "react-native-toast-message";
+import Clipboard from "../../assets/Clipboard.png";
 
 export function Home() {
   const [tasks, setTasks] = React.useState<string[]>([]);
   const [task, setTask] = React.useState<string>("");
 
   function handleCreate(task: string): void {
+    if (tasks.find((el) => el === task)) {
+      // FIXME: Toast not displaying
+      Toast.show({
+        type: "error",
+        text1: "Opa!",
+        text2: "Você já cadastrou uma tarefa igual a essa",
+      });
+      return;
+    }
     setTasks((prev) => [...prev, task]);
   }
-  const handleMarkAsDone = () => {};
-  const handleDelete = () => {};
+  function handleMarkAsDone() {}
+
+  function handleDelete(pressedTask: string) {
+    const removeTask = tasks.filter((el) => el !== pressedTask);
+
+    setTasks((prev) => [...removeTask]);
+  }
 
   React.useEffect(() => {
     console.info(tasks);
@@ -47,15 +64,34 @@ export function Home() {
             <Text style={{ fontSize: 24, color: "#FFF" }}>+</Text>
           </TouchableOpacity>
         </View>
-        <ScrollView>
-          {tasks?.map((task) => (
-            <View style={styles.todo_card} key={task}>
-              <Checkbox status="checked" />
-              <Text style={styles.text}>{task}</Text>
-              <IconButton icon={"delete"} iconColor="#E25858" />
+        {/* TODO: Get rid of this big hunk of code and use FlatList */}
+        {tasks.length > 0 ? (
+          <ScrollView>
+            {tasks?.map((task) => (
+              <View style={styles.todo_card} key={task}>
+                <Checkbox status="checked" />
+                <Text style={styles.text}>{task}</Text>
+                <IconButton
+                  icon={"delete"}
+                  iconColor="#E25858"
+                  onPress={() => handleDelete(task)}
+                />
+              </View>
+            ))}
+          </ScrollView>
+        ) : (
+          <>
+            <View style={{ alignItems: "center", padding: 5 }}>
+              <Image source={Clipboard} style={{ marginBottom: 10 }} />
+              <Text style={{ color: "white", fontSize: 16, fontWeight: "500" }}>
+                Você ainda não tem tarefas cadastradas
+              </Text>
+              <Text style={{ color: "white", fontSize: 16 }}>
+                Crie tarefas e organize seus items a fazer
+              </Text>
             </View>
-          ))}
-        </ScrollView>
+          </>
+        )}
       </View>
     </>
   );
