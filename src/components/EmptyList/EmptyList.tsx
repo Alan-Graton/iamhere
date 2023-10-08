4ad52e03aa09e432b9820d14a14f8e39d3f17bd8
Alan-Graton <graton.alan@gmail.com>
2023-10-08 00:29:04 -0300

    cleanup: minor code reorg

--- src/components/EmptyList/EmptyList.tsx
+++ src/components/EmptyList/EmptyList.tsx
@@ -2,23 +2,17 @@ import { Image, Text, View } from "react-native";
 
 import Clipboard from "../../assets/Clipboard.png";
 
+import { styles } from "./styles";
+
 export function EmptyList() {
   return (
     <>
       <View style={{ alignItems: "center", padding: 5 }}>
         <Image source={Clipboard} style={{ marginBottom: 10 }} />
-        <Text
-          style={{
-            color: "white",
-            fontSize: 14,
-            fontFamily: "Inter_500Medium",
-          }}
-        >
+        <Text style={styles.empty_list_title}>
           Você ainda não tem tarefas cadastradas
         </Text>
-        <Text
-          style={{ color: "white", fontSize: 14, fontFamily: "Inter_300Light" }}
-        >
+        <Text style={styles.empty_list_text}>
           Crie tarefas e organize seus items a fazer
         </Text>
       </View>
---
+++ src/components/EmptyList/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  empty_list_title: {
+    color: "white",
+    fontSize: 14,
+    fontFamily: "Inter_500Medium",
+  },
+  empty_list_text: {
+    color: "white",
+    fontSize: 14,
+    fontFamily: "Inter_300Light",
+  },
+});
